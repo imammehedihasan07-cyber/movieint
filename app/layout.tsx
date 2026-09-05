f5c5d429b2639ba3f98c53a883d8f7538eb71866
Imam Mehedi Hasan <imammehedihasan07@gmail.com>
2026-09-06 00:32:49 +0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,6 @@ export const metadata: Metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL || "https://movieint.vercel.app"
   ),
-
-verification: {
-    google: "HfvzuzUidPVymGZ1FiWVjpx8DwsctgAXUB-YsFBMFJQo",
-  },
   
   title: {
     default: "MOVIEINT — Cinematic Intelligence & Narrative DNA Engine",

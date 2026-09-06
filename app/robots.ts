5aaca5a3ada0ed2981966221620266eb39d7e8a1
Imam Mehedi Hasan <imammehedihasan07@gmail.com>
2026-09-06 19:24:54 +0600

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,11 +4,28 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://www.movieint.com";
 
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: ["/api/"],
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/search*",
+          "/watchlist*",
+          "/_next/",
+          "/static/",
+        ],
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+        disallow: [
+          "/api/",
+          "/search*",
+          "/watchlist*",
+        ],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }

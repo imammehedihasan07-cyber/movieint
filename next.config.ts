414701e87d8292b3c8a36945aa7d7f59fcbd9a16
Imam Mehedi Hasan <imammehedihasan07@gmail.com>
2026-09-06 20:00:49 +0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "image.tmdb.org",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.tmdb.org",
         pathname: "/**",
       },
     ],
